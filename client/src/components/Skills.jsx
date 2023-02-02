59f22433601a591d7f89675e2e92dc8c57ac5687
Cheryl Casas <Cherylccasas@gmail.com>
2023-02-01 21:43:08 -0700

    Added component and skeleton architecture for website

---
+++ client/src/components/Skills.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+import Masthead from "./Masthead.jsx"
+
+const Skills = () => {
+  return (
+    <div>
+      <Masthead/>
+      <h1>SKILLS PAGE</h1>
+    </div>
+  )
+}
+
+export default Skills
---
+++ client/src/components/VisualArtsProjects.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+import Masthead from "./Masthead.jsx"
+
+const VisualArtsProjects = () => {
+  return (
+    <div>
+      <Masthead/>
+      <h1> VISUAL ARTS PAGE </h1>
+    </div>
+  )
+};
+
+export default VisualArtsProjects
