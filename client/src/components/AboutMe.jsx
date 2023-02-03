715cbbd487d2e5cc190f61f812c4ba6148296db9
Cheryl Casas <Cherylccasas@gmail.com>
2023-02-02 21:55:57 -0700

    Adding emojis, switch theme background, animation header

--- client/src/components/AboutMe.jsx
+++ client/src/components/AboutMe.jsx
@@ -4,11 +4,15 @@ import Masthead from "./Masthead.jsx"
 
 const AboutMe = () => {
     return (
-      <div class="AboutMe page">
+      <div class='AboutMe, component'>
         <Masthead/>
+        <div class='bufferDiv'>
+        <div class="body">
         <h1>ABOUT ME PAGE</h1>
-        <p>I am a fullstack engineer, curious thinker and boundless dreamer.  Passionate about learning, puzzles, technology and creating, Software Engineering is the perfect profession for me.  Through technology, I channel my passions through applying my skills in designing and creating attractive and fluid UI, building api's, mapping databases and applying data, learning new technologies and building the bridges to connect all these key components together.
+        <p class="myIntro">I am a fullstack engineer, curious thinker and boundless dreamer.  Passionate about learning, puzzles, technology and creating, Software Engineering is the perfect profession for me.  Through technology, I channel my passions through applying my skills in designing and creating attractive and fluid UI, building api's, mapping databases and applying data, learning new technologies and building the bridges to connect all these key components together.
         </p>
+        </div>
+        </div>
       </div>
     )
 
--- client/src/components/LandingPage.jsx
+++ client/src/components/LandingPage.jsx
@@ -2,9 +2,13 @@ import React from "react"
 // import {Link}  from "react-router-dom";
 // import Button from 'react-bootstrap/Button';
 import Masthead from "./Masthead.jsx"
+import Me from "../images/Me.jpg"
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 const LandingPage = () => {
-  const qualities = ['creative creature', 'cosplayer', 'artist', 'dog mama', 'food enthusiast', 'avid learner', 'prop designer']
+  const qualities = [`👩‍🎨 creative creature	👩‍🎨`, '👩‍🎤 cosplayer 👩‍🎤	', '🎨 artist 🎨', '🦮 dog mama 🦮', '🍔 food enthusiast 🍔', '🤓 avid learner 🤓', '🎭 prop designer 🎭	', `🪷 novice gardener 🪷`, '👹 costume maker 👹	', '✨ dreamer ✨', '🥛 optimist 🥛	']
 
   const [quality, setQuality] = React.useState("");
 
@@ -19,10 +23,35 @@ const LandingPage = () => {
   }, [shuffle])
 
   return(
-    <div class="LandingPage">
+    <div class="LandingPage, component">
       <Masthead/>
+      <div class='bufferDiv'>
       <div class='body'>
-        <h1>Hello there! I'm Cheryl, software engineer and {quality}.</h1>
+        <h1 class="animate__animated animate__backInLeft">
+          Hello there!
+        </h1>
+          <br/>
+        <Container fluid>
+          <Row>
+            <Col md="8">
+              <h2>I'm Cheryl, <br/>
+               software engineer and <br/>
+               {quality}
+               <br/>
+               <br/>
+               Thanks for visiting!
+              </h2>
+            </Col>
+            <Col md="4">
+              <img
+              className="myPhoto"
+              src={Me}
+              // height="100%" width="100%"
+              alt="Chery, herself"/>
+            </Col>
+          </Row>
+        </Container>
+      </div>
       </div>
     </div>
   )
